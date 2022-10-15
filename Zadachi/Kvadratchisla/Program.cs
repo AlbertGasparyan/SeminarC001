014a0b51bd363a7822656e80b5869b34b061bd83
Albert <mona-m1@ya.ru>
2022-10-15 16:40:16 +0600

    chisla

---
+++ Zadachi/Kvadratchisla/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA * numberA == numberB) 
+Console.Write($"Число {numberA} является квадратом числа {numberB}");
+else
+Console.WriteLine("Выражение не является вверным!");
