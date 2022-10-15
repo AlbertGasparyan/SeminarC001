0e2e193949a6cfa7774bedbc0ab8737794ffbc7f
Albert <mona-m1@ya.ru>
2022-10-15 16:47:36 +0600

    Days

---
+++ Zadachi/Days/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число от 1 до 7 ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number==1) Console.Write("Понедельник");
+if(number==2) Console.Write("Вторник");
+if(number==3) Console.Write("Среда");
+if(number==4) Console.Write("Четверг");
+if(number==5) Console.Write("Пятница");
+if(number==6) Console.Write("Суббота");
+if(number==7) Console.Write("Воскресенье");
+if(number<0) Console.Write("Нужно ввести число от 1 до 7!");
+if(number>7) Console.Write("Нужно ввести число от 1 до 7!");
