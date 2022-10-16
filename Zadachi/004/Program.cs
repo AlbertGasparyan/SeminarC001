29b9da5cb35ef9e94ab24ab6483b2b024a451163
Albert <mona-m1@ya.ru>
2022-10-16 14:26:38 +0600

    004

---
+++ Zadachi/004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while(count<=number)
+{
+    if(count%2==0)
+    Console.WriteLine(count);
+    count++;
+}
